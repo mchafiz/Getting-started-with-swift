b674a2fb3eff1d2204b61f48d1c73ead68ade4fb
muhammad chaerul hafiz <mchaerulhafiz@gmail.com>
2026-06-29 09:37:52 +0700

    Initial Commit

---
+++ WoldBites/WoldBitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoldBitesApp.swift
+//  WoldBites
+//
+//  Created by Muhammad Chaerul Hafiz on 29/06/26.
+//
+
+import SwiftUI
+
+@main
+struct WoldBitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
